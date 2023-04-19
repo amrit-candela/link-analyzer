06dbc9db66e58c0459d0631a362fd94fd1401bd0
Amrit Raj <rajamrit.cs@gmail.com>
2023-04-19 13:43:06 +0530

    Code refactor

--- app/src/main/java/com/linkanalyzer/linkanalyzer/ui/saveData/SaveDataFragment.java
+++ app/src/main/java/com/linkanalyzer/linkanalyzer/ui/saveData/SaveDataFragment.java
@@ -1,21 +1,20 @@
-package com.linkanalyzer.linkanalyzer.Fragments;
+package com.linkanalyzer.linkanalyzer.ui.saveData;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.linkanalyzer.linkanalyzer.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link localScanFragment#newInstance} factory method to
+ * Use the {@link SaveDataFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class localScanFragment extends Fragment {
+public class SaveDataFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +25,7 @@ public class localScanFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public localScanFragment() {
+    public SaveDataFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +35,11 @@ public class localScanFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment localScanFragment.
+     * @return A new instance of fragment SaveDataFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static localScanFragment newInstance(String param1, String param2) {
-        localScanFragment fragment = new localScanFragment();
+    public static SaveDataFragment newInstance(String param1, String param2) {
+        SaveDataFragment fragment = new SaveDataFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +60,6 @@ public class localScanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_local_scan, container, false);
+        return inflater.inflate(R.layout.fragment_save_data, container, false);
     }
 }
